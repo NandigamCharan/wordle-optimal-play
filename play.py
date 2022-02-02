ed4fa3c985f71f54524331c48f57c7f4546738a6
Nandigam Charan <57171014+NandigamCharan@users.noreply.github.com>
2022-02-02 20:29:58 +0530

    02-02-2022

--- play.py
+++ play.py
@@ -1,8 +1,37 @@
-class Easy_Wordle:
-    def __init__():
-        pass
-        
-class Hard_Wordle:
-    def __init__():
+import imp
+import typer
+import json
+from wordle import *
+
+def dictionary():
+    filepath = "./vocab.json"
+    f = open(filepath)
+    j = json.load(f)
+    f.close()
+    return set(j["wordle"]["vocab"]).union(set(j["wordle"]["other"]))
+
+def letter_freq(words):
+    table = {}           # freq table
+    for word in words:
+        for i in word:
+            if i not in table:
+                table[i]  = 0
+            table[i] += 1
+    return table
+
+
+hard = False
+all_words = False
+wordle = Wordle(False, True)
+
+def play_easy(wordle):
+    def best(words):
         pass
     
+    picks = {}
+    words = dictionary()
+    freq = sorted(list(letter_freq(words).items()), key= lambda x: x[1])
+    print(freq)
+
+
+print(play_easy(wordle))
--- wordle.py
+++ wordle.py
@@ -5,29 +5,38 @@ class Attempt:
     def __init__(self, error, output):
         self.error = error
         self.output = output
-        
+
+
 class Wordle:
-    def __init__(self, all_words:bool=False, hard:bool=False):
+    def __init__(self, all_words: bool=False, hard: bool=False):
         self.word = self.random_word()
         self.guesses = []
         self.all_words = all_words
         self.hard = hard
+        self.vocab = None
+        self.dictionary = None 
+        
+        self.load_words()
+        self.random_word()
     
     def load_words(self):
         f = open("./vocab.json")
         j = json.load(f)
         f.close()
-        if self.all_words:
-            return j["wordle"]["vocab"] + j["wordle"]["other"]
-        return j["wordle"]["vocab"]
+        if self.all_words == True:
+            return set(j["wordle"]["vocab"] + j["wordle"]["other"])
+        return set(j["wordle"]["vocab"])
     
     def random_word(self):
-        words = self.load_words()
-        self.word = random.choice(words)
+        return random.choice(self.vocab)
     
     def valid(self, w:str):
         if len(w) != len(self.word):
             return False
+        
+        if w not in self.vocab and w not in self.dictionary:
+            return False
+        
         if self.hard and len(self.guesses) > 0:
             for i in range(len(w)):
                 if self.guesses[-1][i] == self.word[i] and self.word[-1][i] != w[i]:
@@ -38,6 +47,7 @@ class Wordle:
         """ if not vaild returns []"""
         if self.vaild(w):
             x = self.robo_guess(w)
+            return x
     
     def robo_guess(self, w:str):
         """This Function assumes input is always vaild
@@ -55,7 +65,7 @@ class Wordle:
             else:
                 out.append(0)
     
-        # checks if the word is at the position 
+        # checks if weather that letter is at the position 
         for i in len(out):
             if w[i] == self.word[i]:
                 out[i] = 2
